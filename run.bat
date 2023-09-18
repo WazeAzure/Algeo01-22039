77ae9578aa7bb69cdfd17c27972abf44453f0676
novelxv <novelyaputriramadhani11@gmail.com>
2023-09-18 17:38:41 +0700

    add comment di run.bat

--- run.bat
+++ run.bat
@@ -1,7 +1,10 @@
 cls
 
-javac -d bin\ src\*.java
+javac -d bin\ src\*.java 
+:: untuk compile semua file di source (src) menjadi binary (bin)
 
 jar cvmf MANIFEST.MF FinalApp.jar bin/*.class
+:: output berupa FinalApp.jar, butuh file manifest dan class dari bin
 
 java -jar FinalApp.jar
+:: running file FinalApp.jar
