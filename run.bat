591bf29aff1507bc50c063f83db9b0fe4ba67070
WazeAzure <bryan.rich0604@gmail.com>
2023-09-26 19:47:39 +0700

    Fix: Script for Bat File

--- run.bat
+++ run.bat
@@ -1,11 +1,10 @@
 cls
 
-javac -d bin\ src\*.java 
+javac -d bin\ .\src\MyApp.java .\src\Library\* .\src\PersoalanSPL\*
 :: untuk compile semua file di source (src) menjadi binary (bin)
 
-jar cvmf MANIFEST.MF FinalApp.jar bin/*.class
+jar cvmf MANIFEST.MF FinalApp.jar bin\MyApp.class bin\Library\* bin\PersoalanSPL\* 
 :: output berupa FinalApp.jar, butuh file manifest dan class dari bin
 
 java -jar FinalApp.jar
 :: running file FinalApp.jar
-halohao
