7eba1ea53d898844d3fb7ea0f6c39e3b01209293
dianatrihy <dianatrihy@gmail.com>
2023-10-02 04:59:26 +0700

    interpolasi polinom

--- src/PersoalanSPL/InterpolasiPolinomial.java
+++ src/PersoalanSPL/InterpolasiPolinomial.java
@@ -1,7 +1,45 @@
 package PersoalanSPL;
 
+import Library.Matrix;
+import Library.MetodeEliminasi;
+import Library.OperasiDasarMatrix;
+import java.lang.Math;
+
 public class InterpolasiPolinomial {
-    public InterpolasiPolinomial(){
-        System.out.println("Halo aku bagian mbakdi");
+    OperasiDasarMatrix Operate = new OperasiDasarMatrix();
+    MetodeEliminasi ME = new MetodeEliminasi();
+
+    // Jika input dari keyboard:
+    public Matrix InputtoMatrix (int n){
+        // n adalah banyaknya titik yang diketahui
+        Matrix m = new Matrix();
+        Operate.readMatrix(m, n, 2);
+        return m;
+    }
+
+    public Matrix MatrixtoMatrixInt (Matrix m) {
+        Matrix inter = new Matrix();
+        Operate.createMatrix(inter, m.get_ROW_EFF(), m.get_ROW_EFF()+1);
+        int i, j;
+        for (i = 0; i < inter.get_ROW_EFF(); i++){
+            for (j = 0; j < inter.get_COL_EFF()-1; j++){
+                inter.set_ELMT(i, j, Math.pow(m.get_ELMT(i, 0), j));
+            }
+        }
+        for (i = 0; i < inter.get_ROW_EFF(); i++){
+            inter.set_ELMT(i, inter.get_COL_EFF()-1 , m.get_ELMT(i, 1));
+        }
+        return inter;
+    }
+
+    public double Interpolasi (Matrix m, double x){
+        // memperkirakan nilai pada titik x
+        Matrix SPL = ME.Gauss(m);
+        int i; 
+        double y = 0;
+        for(i = 0; i < SPL.get_ROW_EFF(); i++){
+            y += Math.pow(x, i) * SPL.get_ELMT(i, 0);
+        }
+        return y;
     }
 }
