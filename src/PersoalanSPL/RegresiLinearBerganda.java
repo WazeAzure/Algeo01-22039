cc8c8eb0a572d0bcd3e0d501b753fc200087b04c
novelxv <novelyaputriramadhani11@gmail.com>
2023-10-05 00:53:43 +0700

    RegresiLinearBerganda jadi yey

--- src/PersoalanSPL/RegresiLinearBerganda.java
+++ src/PersoalanSPL/RegresiLinearBerganda.java
@@ -1,5 +1,7 @@
 package PersoalanSPL;
 
+import java.util.Scanner;
+
 import Library.Determinan;
 import Library.Matrix;
 import Library.MetodeEliminasi;
@@ -11,10 +13,12 @@ public class RegresiLinearBerganda{
     Determinan DET = new Determinan();
     MetodeEliminasi ME = new MetodeEliminasi();
     OperasiDasarMatrix ODM = new OperasiDasarMatrix();
-    // MyApp MENU = new MyApp();
+    Scanner sc = new Scanner(System.in);
+
+    /* *** HELPER FUNCTIONS *** */
 
     public double sumOfAllElmtInCol(Matrix m, int idxCol){
-        /* M */
+        /* Menghitung jumlah semua elemen pada suatu kolom */
         double sum = 0;
         for (int i = 0; i < m.get_ROW_EFF(); i++){
             sum += m.get_ELMT(i, idxCol);
@@ -30,7 +34,7 @@ public class RegresiLinearBerganda{
     }
 
     public double sumOfProductTwoCols(Matrix m, int idxCol1, int idxCol2){
-        /* M */
+        /* Menghitung jumlah dari hasil kali setiap 2 elemen sebaris dalam 2 kolom */
         double sum = 0;
         for (int i = 0; i < m.get_ROW_EFF(); i++){
             sum += (m.get_ELMT(i, idxCol1) * m.get_ELMT(i, idxCol2));
@@ -38,88 +42,71 @@ public class RegresiLinearBerganda{
         return sum;
     }
 
+    /* *** CONSTRUCTOR *** */
+
     public RegresiLinearBerganda(){
-    // public static void main(String[] args) {
-        System.out.println("Halo aku bagian novel");
+        System.out.println("\nHalo aku bagian novel");
         
-        // Matrix m = new Matrix();
-        
-        // MENU.jenis_input();
-        // int input_type = jenis_input();
-        // if (input_type == 1){
-        //     // input dari keyboard
-        //     ODM.createMatrix(m, );
-        // }
-        // else{
-        //     // input dari file
-        // }
-        
-        // input matrix awal:
-        // x0,0 ... x(k-1),0 y0
-        // ...
-        // x(n-1),0 ... x(n-1),(k-1) y(n-1)
-        
-
-
-        // n banyak baris input, k banyak variabel x
-        int n = 20, k = 3; // input matrix n x (k+1) (ex: 20 x 4)
-        // n = m.getROW, k = m.getCol - 1
-
-        // CONTOH: (n dan k fix yang dari spek)
+        // Input dari keyboard
+        int k, n;
+        System.out.println("Masukkan jumlah peubah x:");
+        k = sc.nextInt();
+        System.out.println("Masukkan jumlah sampel:");
+        n = sc.nextInt();
+        System.out.println("Masukkan data (dalam bentuk matriks):");
         Matrix m = new Matrix();
         ODM.createMatrix(m, n, k + 1);
         ODM.readMatrix(m, n, k + 1);
 
+        // Bentuk Normal Estimation Equation dalam matrix
         Matrix NormalEqMatrix = new Matrix();
         ODM.createMatrix(NormalEqMatrix, k + 1, k + 2);
-        
         // Set baris 1
         NormalEqMatrix.set_ELMT(0, 0, n);
         for (int j = 1; j < NormalEqMatrix.get_COL_EFF(); j++){
             NormalEqMatrix.set_ELMT(0, j, sumOfAllElmtInCol(m, j-1));
         }
-
-        System.out.println("--------CEK Baris 1 -----------");
-        ODM.displayMatrix(NormalEqMatrix);
-
-        // // Set baris pengali
-        // Matrix BarisPengali = new Matrix();
-        // ODM.createMatrix(BarisPengali, 1, NormalEqMatrix.get_COL_EFF());
-        // BarisPengali.set_ELMT(0, 0, 1);
-        // for (int j = 1; j < NormalEqMatrix.get_COL_EFF(); j++){
-        //     BarisPengali.set_ELMT(0, j, NormalEqMatrix.get_ELMT(0, j));
-        // }
-
-        // System.out.println("--------CEK Baris Pengali -----------");
-        // ODM.displayMatrix(BarisPengali);
-
         // Set kolom 1
         for (int i = 1; i < m.get_ROW_EFF(); i++){
             NormalEqMatrix.set_ELMT(i, 0, sumOfAllElmtInCol(m, i - 1));
         }
-
         // Set baris lainnya
         for (int i = 1; i < NormalEqMatrix.get_ROW_EFF(); i++){
             for (int j = 1; j < NormalEqMatrix.get_COL_EFF(); j++){
                 NormalEqMatrix.set_ELMT(i, j, sumOfProductTwoCols(m, i - 1, j - 1));
             }
         }
-        System.out.println("--------CEK NormalEqMatrix -----------");
-        ODM.displayMatrix(NormalEqMatrix);
 
+        // Dapatkan nilai konstanta persamaan regresi linier berganda
         ME.toEselonRed(NormalEqMatrix);
-        System.out.println("--------CEK NormalEqMatrix to RED -----------");
-        ODM.displayMatrix(NormalEqMatrix);
+        Matrix allConst = new Matrix();
         if (ME.Gauss(NormalEqMatrix) == 1){
-            Matrix allConst = new Matrix();
             allConst = ME.SolveSPLUnik(NormalEqMatrix);
-
-            System.out.println("--------CEK Solution -----------");
-            ODM.displayMatrix(allConst);
         }
-    }
-    // input y, x1, ..., xn
-    // k = 20 n = 3
-    // matrix 20 x 4 jadi (k+1)x(k + 2) 21 x 22
 
+        // Print persamaan hasil regresi linier berganda
+        System.out.println("Persamaan hasil regresi linier berganda:");
+        String[][] persamaan;
+        persamaan = new String[2][1];
+        persamaan[0][0] = "f(x) = " + (Math.round(allConst.get_ELMT(0, 0) * 100000.0) / 100000.0);
+        System.out.printf("f(x) = %.5f", allConst.get_ELMT(0, 0));
+        for (int i = 1; i < allConst.get_ROW_EFF(); i++){
+            persamaan[0][0] += " + " + (Math.round(allConst.get_ELMT(i, 0) * 100000.0) / 100000.0) + " x" + i;
+            System.out.printf(" + %.5f x%d", allConst.get_ELMT(i, 0), i);
+        }
+        
+        // Mencari taksiran nilai fungsi pada x yang diberikan
+        System.out.println("\nTaksiran nilai fungsi:");
+        System.out.printf("Masukkan %d nilai x yang ingin ditaksir: ", k);
+        Matrix taksir = new Matrix();
+        ODM.createMatrix(taksir, k, 1);
+        for (int i = 0; i < taksir.get_ROW_EFF(); i++){
+            taksir.set_ELMT(i, 0, sc.nextDouble());
+        }
+        double hasil = allConst.get_ELMT(0, 0);
+        for (int i = 0; i < allConst.get_ROW_EFF(); i++){
+            hasil += allConst.get_ELMT(i + 1, 0) * taksir.get_ELMT(i, 0);
+        }
+        System.out.printf("Hasil taksirannya adalah %.5f", hasil);
+    }
 }
