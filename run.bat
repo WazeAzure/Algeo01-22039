6c05a2f12107a9fbf10a2fd4609c09896581064a
WazeAzure <bryan.rich0604@gmail.com>
2023-10-05 18:02:30 +0700

    Add: Bicubic Spline Done

--- run.bat
+++ run.bat
@@ -1,10 +1,10 @@
 cls
 
-javac -d bin\ .\src\MyApp.java .\src\Library\* .\src\PersoalanSPL\*
+javac -d bin\ .\src\MyApp.java .\src\Library\* .\src\PersoalanSPL\* && jar cvmf MANIFEST.MF FinalApp.jar bin\MyApp.class bin\Library\* bin\PersoalanSPL\*  && java -jar FinalApp.jar
 :: untuk compile semua file di source (src) menjadi binary (bin)
 
-jar cvmf MANIFEST.MF FinalApp.jar bin\MyApp.class bin\Library\* bin\PersoalanSPL\* 
+
 :: output berupa FinalApp.jar, butuh file manifest dan class dari bin
 
-java -jar FinalApp.jar
+
 :: running file FinalApp.jar
